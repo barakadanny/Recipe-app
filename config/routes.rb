d3dfe587aeeb191df065daad484e67dd89ff2b28
muskan2532150 <rg2532150@gmail.com>
2023-01-04 15:37:11 +0530

    public recipe routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :recipes
   resources :foods,only:[:index,:new,:create,:destroy]
+  get 'public_recipes', to: 'recipes#public_recipes'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

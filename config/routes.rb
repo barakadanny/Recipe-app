5a8cea872ac6728b2d965028016919de32954b59
muskan2532150 <rg2532150@gmail.com>
2023-01-04 14:42:50 +0530

    Add route for food

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :recipes
+  resources :foods,only:[:index,:new,:create,:destroy]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

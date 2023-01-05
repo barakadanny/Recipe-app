e50226e7161ba895da094a03fa2f586b5cbfdc23
muskan2532150 <rg2532150@gmail.com>
2023-01-05 15:11:17 +0530

    Add recipe_food route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  resources :recipes
+  resources :recipes do
+    resources :recipe_foods
+  end
   resources :foods,only:[:index,:new,:create,:destroy]
   get 'public_recipes', to: 'recipes#public_recipes'
+  get 'shopping_list', to: 'recipes#shopping_list'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

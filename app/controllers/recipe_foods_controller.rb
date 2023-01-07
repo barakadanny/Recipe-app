5f76a8877aa2914db6f98d95421da44d1ad259d5
muskan2532150 <rg2532150@gmail.com>
2023-01-07 17:39:10 +0530

    Update table,styles, form

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,5 +1,7 @@
 class RecipeFoodsController < ApplicationController
+  before_action :setup, only: [:edit,:new,:destroy]
   access user: :all, admin: :all
+
   def index
     @recipe=Recipe.joins(:recipe_foods).find(params[:recipe_id])
     @food=Recipe.joins(:foods).find(params[:recipe_id])
@@ -23,6 +25,12 @@ class RecipeFoodsController < ApplicationController
     @food_items=selected(@foods)
   end
 
+  def edit
+    @recipe = Recipe.find(params[:recipe_id])
+    @foods = Food.where(user_id: current_user.id)
+    @food_items=selected(@foods)
+  end
+
   def create
     @recipe_foods = RecipeFood.new(recipe_food_params)
     respond_to do |format|
@@ -38,37 +46,29 @@ class RecipeFoodsController < ApplicationController
     end
   end
 
-  def edit
-    @recipe = Recipe.find(params[:recipe_id])
-    @foods = Food.where(user_id: current_user.id)
-    @food_items=selected(@foods)
-  end
-
   def update
-     @recipe_foods=RecipeFood.find(params[:id])
-     respond_to do |format|
-       # rubocop:disable Style/IfInsideElse
-       if @recipe_foods.update(recipe_food_params)
-         format.html { redirect_to recipe_url(@recipe_foods.recipe_id), notice: 'Recipe Ingridents was successfully updated.' }
-       else
-         format.html { render :edit, status: :unprocessable_entity }
-       end
-        # rubocop:enable Style/IfInsideElse
-     end
-  end
-
-  def destroy
-    @recipe_foods = RecipeFood.find(params[:id])
-    @recipe=Recipe.find(params[:recipe_id])
-    if @recipe.user_id != current_user.id
-      redirect_to recipe_url(@recipe), notice: 'You are not the owner of this recipe.'
-    else
-      @recipe.destroy
-      respond_to do |format|
-        format.html { redirect_to recipes_url, notice: 'Recipe was successfully destroyed.' }
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = RecipeFood.find(params[:id])
+    respond_to do |format|
+      if @recipe_food.update(recipe_food_params)
+        format.html { redirect_to @recipe, notice: 'Recipe food was successfully updated.' }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
       end
     end
   end
+
+  def destroy
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food.destroy
+
+    # redirect_to @recipe
+    respond_to do |format|
+      format.html { redirect_to @recipe, notice: 'Recipe food was successfully destroyed.' }
+    end
+  end
+  
+ def show; end
 
   private
   def recipe_food_params
@@ -84,6 +84,10 @@ class RecipeFoodsController < ApplicationController
     end
     return food_items
   end
+
+  def setup
+    @recipe_food = RecipeFood.find(params[:id])
+  end 
 
   helper_method :selected
 end

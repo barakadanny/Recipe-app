39ec024f1dbe84d42b64c65eef74b1a256e7dc0c
muskan2532150 <rg2532150@gmail.com>
2023-01-07 14:24:06 +0530

    Remove unneccessary comments, create Recipe Food edit action

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -20,10 +20,7 @@ class RecipeFoodsController < ApplicationController
   def new
     @recipe = Recipe.find(params[:recipe_id])
     @foods = Food.where(user_id: current_user.id)
-    @food_items = []
-    @foods.each do |food|
-      @food_items << [food.name, food.id]
-    end
+    @food_items=selected(@foods)
   end
 
   def create
@@ -41,9 +38,42 @@ class RecipeFoodsController < ApplicationController
     end
   end
 
+  def edit; end
+
+    def update
+    @recipe = Recipe.find(params[:recipe_id])
+    @foods = Food.where(user_id: current_user.id)
+    @food_items=selected(@foods)
+    @recipe_foods = RecipeFood.new(recipe_food_params)
+    respond_to do |format|
+      if @recipe.user_id != current_user.id
+        format.html { redirect_to recipe_url(@recipe_foods.recipe_id), notice: 'You are not the owner of this recipe.' }
+      else
+        # rubocop:disable Style/IfInsideElse
+        if @recipe_foods.update(recipe_food_params)
+          format.html { redirect_to recipe_url(@recipe_foods.recipe_id), notice: 'Recipe Ingridents was successfully updated.' }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+        end
+        # rubocop:enable Style/IfInsideElse
+      end
+    end
+  end
+
+  private
   def recipe_food_params
     params
       .require(:recipe_food)
       .permit(:quantity, :recipe_id, :food_id)
   end
+  
+  def selected(foods)
+    food_items = []
+    foods.each do |food|
+      food_items << [food.name, food.id]
+    end
+    return food_items
+  end
+
+  helper_method :selected
 end

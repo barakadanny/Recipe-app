28c6829fc01343bb867e0a672cbeced8078a78b1
Danny <barakadan421@gmail.com>
2023-01-07 14:00:04 +0300

    Add recipe_food model test

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe RecipeFood, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe RecipeFood do
+    describe 'associations' do
+      it 'belongs to food' do
+        food = Food.new
+        recipe_food = RecipeFood.new(food: food)
+        expect(recipe_food.food).to eq(food)
+      end
+  
+      it 'belongs to recipe' do
+        recipe = Recipe.new
+        recipe_food = RecipeFood.new(recipe: recipe)
+        expect(recipe_food.recipe).to eq(recipe)
+      end
+    end
+  end
 end

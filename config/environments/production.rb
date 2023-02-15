d1e87e39ad89637025ea416bc640abde6a4ba2fe
Danny <barakadan421@gmail.com>
2023-02-15 15:35:18 +0300

    Dev link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
-  config.hosts << "recipe-app-production-650e.up.railway.app"
+  # config.hosts << "recipe-app-production-650e.up.railway.app"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
